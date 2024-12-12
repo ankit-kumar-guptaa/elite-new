402ac938c9c1a836ae041f73e2985ee8099a6a87
ANKIT KUMAR GUPTA <65163759+ankit-kumar-guptaa@users.noreply.github.com>
2024-12-12 20:48:42 +0530

    Enhance About page with new sections including inner banner, callout banner, company history, core values, and counters. Remove outdated partner slider from homepage and update inner banner image for improved visual appeal.

---
+++ include/partner.php
@@ -0,0 +1,30 @@
+	<!--
+			=====================================================
+				Partner Slider
+			=====================================================
+			-->
+			<div class="partner-section bg-color">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-4 col-12">
+							<h6>OUR <br>PARTNERS</h6>
+						</div>
+						<div class="col-md-9 col-sm-8 col-12">
+							<div class="partner-slider">
+								<div class="item"><img src="images/client/1.png" alt=""></div>
+								<div class="item"><img src="images/client/2.png" alt=""></div>
+								<div class="item"><img src="images/client/7.png" alt=""></div>
+								<div class="item"><img src="images/client/4.png" alt=""></div>
+								<div class="item"><img src="images/client/5.png" alt=""></div>
+								<div class="item"><img src="images/client/3.png" alt=""></div>
+								<div class="item"><img src="images/client/9.png" alt=""></div>
+								<div class="item"><img src="images/client/10.png" alt=""></div>
+								<div class="item"><img src="images/client/11.png" alt=""></div>
+								<div class="item"><img src="images/client/12.png" alt=""></div>
+								<div class="item"><img src="images/client/13.png" alt=""></div>
+								<!-- <div class="item"><img src="images/client/14.png" alt=""></div> -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> <!-- /.partner-section -->
