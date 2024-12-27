11fe81a0924083191eedefb07afc065d2cba48d9
ANKIT KUMAR GUPTA <65163759+ankit-kumar-guptaa@users.noreply.github.com>
2024-12-27 14:37:15 +0530

    Update database password in db.php for enhanced security. This change replaces the previous password with a new one, ensuring better protection of sensitive information. No other files were modified in this commit.

--- include/db.php
+++ include/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost:3306';
 $db = 'recru2l1_submit_form_data';
 $user = 'recru2l1_elite_admin'; // Replace with your database username
-$pass = 'Elite@111@';     // Replace with your database password
+$pass = 'Elite@123@';     // Replace with your database password
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
